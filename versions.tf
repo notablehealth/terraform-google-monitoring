5749447fdc6c30135300492e77263a28cd8fdcca
Brandon Boudrias <brandon.boudrias@notablehealth.com>
2026-01-09 17:07:47 -0800

    support log_check_failures and notification_prompts

--- versions.tf
+++ versions.tf
@@ -1,10 +1,6 @@
 
 terraform {
   required_version = ">= 1.5.7"
-  #required_providers {
-  #  google = {
-  #    source  = "hashicorp/google"
-  #    version = ">= 4.84"
-  #  }
-  #}
+  # No required google version because
+  # the root module doesn't use the provider.
 }
